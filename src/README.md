53f116f91df29baa14fd14eef83a31ae8de6df91
Nathan <25822892+Highway-Kebabbery@users.noreply.github.com>
2024-06-09 00:32:21 -0400

    Initial commit. Remaining dev done on Android.

---
+++ src/README.md
@@ -0,0 +1,34 @@
+How to Use:
+
+Download Termux from the app store
+run pkg update && pkg upgrade (select default options when prompted)
+run pkg install python
+run pkg install python-pip
+run pip install selenium beautifulsoup4
+
+install firefox browser from app store
+
+
+
+Where I left off:
+It looks like I need a file manager to be able to poke around my android phone. They make them with no rooting required. This may not be necessary for describing how to install and use the app.
+I need to figure out how to have easy control over file locations on the phone so that I can troubleshoot and get everything set up.
+It looks like GeckoDriver may work as is on the phone.... Or not. IDFK yet.
+
+install FX file explorer from the app store: https://play.google.com/store/apps/details?id=nextapp.fx
+
+
+
+
+
+
+
+Learned:
+If there's too much HTML, you can't print all to the console. Save to a file to see it all.
+Learned about docstrings
+Got to practice building Python classes
+
+
+NOTES:
+Have the Android notification take you to the jobs page when clicked (even when no jobs available).
+HERE'S A BIG NOTE: THIS COMPARES THE OLD RESULTS TO THE NEW TO DETERMINE IF  AJOB WAS POSTED. IF THEY DELETE A JOB, IT'LL TELL ME A JOB WAS POSTED.
---
+++ src/ScrapeJobs.py
@@ -0,0 +1,144 @@
+import os
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+
+class CompanyJobsFinder():
+    """A class for finding job listings available at a company."""
+
+    __driver = None
+    __company_name = ''
+    __url = ''
+    __target_tag = ''
+    __target_attribute_value = ''
+    __current_jobs = []
+    __previous_jobs = None
+
+    def __init__(self, company_name, url, target_tag, target_attribute_value):
+        self.__company_name = company_name
+        self.__url = url
+        self.__target_tag = target_tag
+        self.__target_attribute_value = target_attribute_value
+        self.set_firefox_driver()
+    
+    def set_firefox_driver(self, mobile=True):
+        """Set up the web driver
+
+        Args:
+            mobile (bool, optional): I set the script/project up to run both on Win64 (for basic configuration) and on Linux
+            because the script is run through Termux on Android, which provides a Linux-based environment. When troubleshooting
+            on Win64, don't forget to add the <mobile=False> argument when called in __init__. Defaults to True.
+        """
+
+        if mobile == True:
+            gecko_driver_path = './Drivers/linux-aarch64/geckodriver'
+        else:
+            gecko_driver_path = './Drivers/win64/geckodriver.exe'
+            service = FirefoxService(executable_path=gecko_driver_path)
+            options = webdriver.FirefoxOptions()
+            options.add_argument("--headless")
+            self.__driver = webdriver.Firefox(service=service, options=options)
+
+    @property
+    def company_name(self):
+        """Getter for self.__company_name
+
+        Returns:
+            string: company name
+        """
+        return self.__company_name
+    
+    @property
+    def previous_jobs(self):
+        """Getter for self.__previous_jobs
+
+        Returns:
+            .json: List of jobs as of the last search.
+        """
+        return self.__previous_jobs
+
+    def set_previous_jobs(self):
+        """Setter for self.__previous_jobs
+        """
+        if os.path.exists('./Data/' + str(self.__company_name) + '.json'):
+            with open('./Data/' + str(self.__company_name) + '.json', 'r') as file:
+                self.__previous_jobs = json.load(file)
+                file.close()
+
+    # Get and set current jobs.
+    @property
+    def current_jobs(self):
+        """Getter for self.__current_jobs
+
+        Returns:
+            List: Jobs currently posted on company website.
+        """
+        return self.__current_jobs
+    
+    def set_current_jobs_by_class(self, child=False):
+        """This method gets a webpage and scrapes it for jobs. It can either operate on
+        tags with unique identifiers, or the immediate child of a tag with a unique identifier.
+
+        Args:
+            child (bool, optional): Sets whether you're looking in the child of the
+            identifiable tag for the job information. Defaults to False.
+
+        Returns:
+            (list): This is the setter method for self.__current_jobs.
+        """
+        
+        self.__driver.get(self.__url)
+
+        # Wait until target element is present to ensure the page has fully loaded
+        try:
+            WebDriverWait(self.__driver, 20).until(
+                EC.presence_of_element_located((By.CLASS_NAME, self.__target_attribute_value))
+            )
+        except Exception as e:
+            print(f"Failed to load job listings: {e}")
+            return []
+
+        html = self.__driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        # Locate job titles.
+        tags = soup.find_all(self.__target_tag, class_=self.__target_attribute_value)
+        if child == False:
+            for target_tag in tags:
+                self.__current_jobs.append({"Title: ": target_tag.string})
+        else:
+            # Pull the string from the child of each uniquely identifiable parent tag.
+            # Only works if there's only one child.
+            for parent_tag in tags:
+                self.__current_jobs.append({"Title: ": parent_tag.find().string})
+
+        self.__driver.quit()
+
+    def set_current_jobs_json(self):
+        """This method saves the current job listings to a .json file for comparison to the old job listings.
+
+        Args:
+            company_name (string): Used to generate filename.
+        """
+        with open('./Data/' + str(self.__company_name) + '.json', 'w') as file:
+            json.dump(self.__current_jobs, file, indent=4)
+            file.close()
+
+def main():
+    Jagex = CompanyJobsFinder('Jagex', 'https://apply.workable.com/jagex-limited/', 'h3', 'styles--3TJHk')
+    Jagex.set_previous_jobs()
+    Jagex.set_current_jobs_by_class(child=True)
+
+    if Jagex.previous_jobs != Jagex.current_jobs:
+        Jagex.set_current_jobs_json()
+        print(f"New job found at {Jagex.company_name}!")
+    else:
+        print("Keep eating ramen noodles.")
+
+if __name__ == '__main__':
+    main()
