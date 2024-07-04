d97b8ad62159c77f1f2108a301064789ea35cd46
Nathan <25822892+Highway-Kebabbery@users.noreply.github.com>
2024-07-04 14:53:33 -0400

    Adding double quotation marks to echo statements.

--- src/scripts/_setup.sh
+++ src/scripts/_setup.sh
@@ -1,8 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+# I'm leaving this code beacuse you can manually set up specific mirrors if you like. It's not always possible to choose a mirror *group* when setting up.
 #setup_mirrors() {
 #    local repo_file="$PREFIX/etc/apt/sources.list"
-#    echo -e \n\n\nSetting up mirrors...\n\n\n
+#    echo -e "\n\n\nSetting up mirrors...\n\n\n"
 #    cat <<EOF > $repo_file
 ## North America mirrors
 #deb https://termux.mentality.rip/termux-main stable main
@@ -19,32 +20,32 @@
 #deb https://termux.mirror1.eu/termux-main stable main
 #deb https://termux.mirror2.eu/termux-main stable main
 #EOF
-#    echo -e \n\n\nMirrors set up successfully.\n\n\n
+#    echo -e "\n\n\nMirrors set up successfully.\n\n\n"
 #}
 
 # Initial configuration
 echo -e "\n\n\nStarting initial configuration...\n\n\n"
-yes | pkg update -y || { echo -e \n\n\nFailed to update packages.\n\n\n; exit 1; }
-yes | pkg upgrade -y || { echo -e \n\n\nFailed to upgrade packages.\n\n\n; exit 1; }
+yes | pkg update -y || { echo -e "\n\n\nFailed to update packages.\n\n\n"; exit 1; }
+yes | pkg upgrade -y || { echo -e "\n\n\nFailed to upgrade packages.\n\n\n"; exit 1; }
 #setup_mirrors
-termux-change-repo || { echo -e \n\n\nFailed to change repo.\n\n\n; exit 1; }
+termux-change-repo || { echo -e "\n\n\nFailed to change repo.\n\n\n"; exit 1; }
 echo -e "\n\n\nCompleted initial configuration.\n\n\n"
 
-echo -e \n\n\nInstalling Termux dependencies...\n\n\n
-yes | pkg install termux-api cronie at python-pip x11-repo firefox geckodriver -y || { echo -e \n\n\nFailed to install Termux dependencies.\n\n\n; exit 1; }
-echo -e \n\n\nTermux dependencies installed.\n\n\n
+echo -e "\n\n\nInstalling Termux dependencies...\n\n\n"
+yes | pkg install termux-api cronie at python-pip x11-repo firefox geckodriver -y || { echo -e "\n\n\nFailed to install Termux dependencies.\n\n\n"; exit 1; }
+echo -e "\n\n\nTermux dependencies installed.\n\n\n"
 
-echo -e \n\n\nInstalling Python dependencies...\n\n\n
-pip install selenium==4.9.1 beautifulsoup4 || { echo -e \n\n\nFailed to install Python dependencies.\n\n\n; exit 1; }
-echo -e \n\n\nPython dependencies installed.\n\n\n
+echo -e "\n\n\nInstalling Python dependencies...\n\n\n"
+pip install selenium==4.9.1 beautifulsoup4 || { echo -e "\n\n\nFailed to install Python dependencies.\n\n\n"; exit 1; }
+echo -e "\n\n\nPython dependencies installed.\n\n\n"
 
-rm -r Job-Scraper-*.tar.gz || { echo -e \n\n\nFailed to remove old .tar.gz file.\n\n\n; exit 1; }
+rm -r Job-Scraper-*.tar.gz || { echo -e "\n\n\nFailed to remove old .tar.gz file.\n\n\n"; exit 1; }
 
-termux-setup-storage || { echo -e \n\n\nFailed to set up Termux Storage permissions.\n\n\n; exit 1; }    # Technically optional. Gives Termux ability to see device storage folder. Need to completely exit Termux to complete setup.
-yes | pkg install termux-services -y || { echo -e \n\n\nFailed to install termux-services.\n\n\n; exit 1; }    # YOU HAVE TO FULLY EXIT THE APP AFTER INSTALLING TERMUX-SERVICES.
+termux-setup-storage || { echo -e "\n\n\nFailed to set up Termux Storage permissions.\n\n\n"; exit 1; }    # Technically optional. Gives Termux ability to see device storage folder. Need to completely exit Termux to complete setup.
+yes | pkg install termux-services -y || { echo -e "\n\n\nFailed to install termux-services.\n\n\n"; exit 1; }    # YOU HAVE TO FULLY EXIT THE APP AFTER INSTALLING TERMUX-SERVICES.
 
-echo -e \n\n!!!!!!!\n
-echo -e ATTENTION: Termux requires a full restart. Completely EXIT the app now.\n
-echo -e !!!!!!!\n\n
-echo -e Upon restart, execute './Job-Scraper-<version number>/Schedule_Job_Scraper.sh to begin running the job scraper.'
+echo -e "\n\n!!!!!!!\n"
+echo -e "ATTENTION: Termux requires a full restart. Completely EXIT the app now.\n"
+echo -e "!!!!!!!\n\n"
+echo -e "Upon restart, execute './Job-Scraper-<version number>/Schedule_Job_Scraper.sh' to begin running the job scraper."
 sleep 30
