05957a88f5af16aac86ea2023b37331dc8c604eb
Highway-Kebabbery <25822892+Highway-Kebabbery@users.noreply.github.com>
2025-04-18 00:23:20 -0400

    Fixing daily notifications.

---
+++ src/scripts/_daily_notif_handler.py
@@ -0,0 +1,16 @@
+import os
+import glob
+
+# Path to the folder where notification .txt files are stored
+folder_path = "/data/data/com.termux/files/home/Job-Scraper-*/src/scripts/daily-scripts"
+
+# Loop through all .txt files in the folder
+for txt_file in glob.glob(os.path.join(folder_path, "*.txt")):
+    try:
+        with open(txt_file, "r") as file:
+            command = file.read().strip()
+
+        os.system(command)
+        os.remove(txt_file)
+    except:
+        continue
